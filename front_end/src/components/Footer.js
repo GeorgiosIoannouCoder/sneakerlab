55299cdff5246318e7eb616a66cb43d9e111f382
GeorgiosIoannouCoder <george_ny3@hotmail.com>
2024-01-10 00:40:20 -0500

    Update Footer.js and LoginScreen.js

--- front_end/src/components/Footer.js
+++ front_end/src/components/Footer.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Footer() {
   return (
     <footer>
-        <Container>
-          <Row>
-            <Col className="text-center py-3">
-                &copy; 2022 SneakerLab New York LLC. All Rights Reserved
-            </Col>
-          </Row>
-        </Container>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            &copy; 2024 SneakerLab New York LLC. All Rights Reserved
+          </Col>
+        </Row>
+      </Container>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
